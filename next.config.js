4eab3cd3a0699df12b4aa9b469493bb43f2f04bc
Farid Amimri <farid.amimri@gmail.com>
2023-06-15 16:08:47 +0200

    Set domains for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  images: {
+    domains: ['cdn.imagin.studio']
+  }
+}
 
 module.exports = nextConfig
