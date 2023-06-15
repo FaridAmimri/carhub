50c5a30d1ebd249668b86bdfda1ddc01512969ac
Farid Amimri <farid.amimri@gmail.com>
2023-06-15 16:05:26 +0200

    Update Custom button component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,24 +2,37 @@
 'use client'
 
 import Image from 'next/image'
+import { CustomButton } from '.'
+
 import { CustomButtonProps } from '@/types'
 
-const CustomButton = ({
-  title,
+const Button = ({
+  isDisabled,
+  btnType,
   containerStyles,
-  handleClick,
-  btnType
-}: CustomButtonProps) => {
-  return (
-    <button
-      disabled={false}
-      type={btnType || 'button'}
-      className={`custom-btn ${containerStyles}`}
-      onClick={handleClick}
-    >
-      <span className={'flex-1'}>{title}</span>
-    </button>
-  )
-}
+  textStyles,
+  title,
+  rightIcon,
+  handleClick
+}: CustomButtonProps) => (
+  <button
+    disabled={isDisabled}
+    type={btnType || 'button'}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className='relative w-6 h-6'>
+        <Image
+          src={rightIcon}
+          alt='arrow_left'
+          fill
+          className='object-contain'
+        />
+      </div>
+    )}
+  </button>
+)
 
-export default CustomButton
+export default Button
